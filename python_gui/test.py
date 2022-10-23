1d40fdc0530505afdd243d880eba081f2532aeae
Tsz Kit Wong <wongt@bu.edu>
2022-10-22 23:17:40 -0400

    Update test.py
    added test for new methods

--- python_gui/test.py
+++ python_gui/test.py
@@ -1,4 +1,12 @@
 import dashboard
 
-# example run
-test_dash = dashboard.DriverDisplay(30, 3000, 700).set_display()
+# example run, test methods
+test_dash = dashboard.DriverDisplay(30, 3000, 700, 1)
+test_dash.setSpeed(40)
+test_dash.setGear(4)
+test_dash.setRevs(5000)
+test_dash.setEnergy(1000)
+
+# values are finalized and displayed onto window to user
+test_dash.set_display()
+test_dash.display_on()
