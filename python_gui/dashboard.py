6f35132b919192636f793bcf45a59f822b6034d0
Tsz Kit Wong <wongt@bu.edu>
2022-10-22 23:14:54 -0400

    Update dashboard.py
    added methods for updating data, added gear to data, added method to turn on display

--- python_gui/dashboard.py
+++ python_gui/dashboard.py
@@ -8,7 +8,7 @@ dashboard.geometry("500x200")  # setting window size
 
 
 class DriverDisplay:
-    def __init__(self, speed, rpm, energy):
+    def __init__(self, speed, rpm, energy, gear):
         """
         initializing data that will be displayed
         """
@@ -16,6 +16,35 @@ class DriverDisplay:
         self.speed = speed
         self.revs = rpm
         self.energy = energy
+        self.gear = gear
+
+
+    def setSpeed(self, mph):
+        """
+        sets the mph to new input
+        """
+        self.speed = mph
+
+
+    def setRevs(self, rpm):
+        """
+        sets the rpm to new input
+        """
+        self.revs = rpm
+
+
+    def setEnergy(self, energy):
+        """
+        sets the energy output to new input
+        """
+        self.energy = energy
+
+
+    def setGear(self, gear):
+        """
+        sets the gear to new input
+        """
+        self.gear = gear
 
 
     def set_display(self):
@@ -27,11 +56,19 @@ class DriverDisplay:
         mph = Label(dashboard, text=f"{self.speed} MPH", font=("Verdana", 30))
         rpm = Label(dashboard, text=f"{self.revs} RPM")
         energy_output = Label(dashboard, text=f"{self.energy} V")
+        gear = Label(dashboard, text=f"Gear: {self.gear}", font=("Verdana", 20))
 
         # position the data labels
+        gear.place(x=205, y=10)
         mph.place(x=185, y=70)
         rpm.place(x=10, y=10)
         energy_output.place(x=450, y=10)
+
+
+    def display_on(self):
+        """
+        Opens up the window
+        """
 
         # bring up display with data
         dashboard.mainloop()
